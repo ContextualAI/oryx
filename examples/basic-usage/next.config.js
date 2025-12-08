d95ab105f0ba82c2f5eb2628da9f8f0f92f1b920
Lingxi Li <lilingxi01@gmail.com>
2025-12-08 13:34:23 -0800

    chore: launch!

--- examples/basic-usage/next.config.js
+++ examples/basic-usage/next.config.js
@@ -1,12 +1,17 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   devIndicators: false,
-  outputFileTracingRoot: __dirname,
+  outputFileTracingRoot: path.resolve(__dirname, "../.."),
   transpilePackages: [
     "@contextualai/oryx-react",
     "@contextualai/oryx-proxy-node",
   ],
+  turbopack: {
+    root: path.resolve(__dirname, "../.."),
+  },
 };
 
 module.exports = nextConfig;
